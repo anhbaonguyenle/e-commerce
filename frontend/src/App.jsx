45b5c1743bc4f2d2a9a06afdf44ee35301b2cdfa
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2024-10-08 22:52:24 +0700

    add route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
@@ -13,14 +14,21 @@ function App() {
   }, [theme]);
 
   return (
-    <div className={`container ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme} />
-      <Hero theme={theme} />
-      <div className="content">
+    <Router>
+      <div className={`container ${theme}`}>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Hero theme={theme} />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/Products" element={<h1>Products</h1>} />
+            <Route path="/Galleries" element={<h1>Galleries</h1>} />
+            <Route path="/About" element={<h1>About</h1>} />
+          </Routes>
+        </div>
+        <Footer theme={theme} />
       </div>
-
-      <Footer theme={theme} />
-    </div>
+    </Router>
   );
 }
 
